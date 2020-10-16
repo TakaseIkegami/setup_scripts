00a34168c2981425fb65be61009af1975ff6c104
takase <takase@lepidum.co.jp>
2020-10-16 15:52:11 +0900

    add: vim_setup.sh

---
+++ vim/vim_setup.sh
@@ -0,0 +1,9 @@
+# vimrcコピー
+cp vimrc ~/.vimrc
+
+# カラースキームのmolokaiをダウンロード
+mkdir ~/.vim/
+mkdir ~/.vim/colors/
+git clone https://github.com/tomasr/molokai.git ./molokai
+cp molokai/colors/molokai.vim ~/.vim/colors/
+
